cbd95c7132502233b268e82082a532c6d78a078e
LucaRiccio <lucariccio888@gmail.com>
2020-09-15 15:29:07 +0200

    Creazione variabile e prova Echo

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relativa lunghezza
+e sostituire la badword passata in GET con tre *. -->
+
+<?php
+  $testo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+  echo $testo;
+?>
