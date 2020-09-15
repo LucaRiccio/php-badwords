ec0d70a3c9d864a5ad1ef942ed3b1fd8b2a8af7d
LucaRiccio <lucariccio888@gmail.com>
2020-09-15 16:12:22 +0200

    Prova get

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ Visualizzare a schermo il paragrafo con la relativa lunghezza
 e sostituire la badword passata in GET con tre *. -->
 
 <?php
-  $testo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-
+  $testo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  $badword = $_GET['dolor'];
   $lunghezza = strlen($testo);
-  
-  echo $testo . $lunghezza;
+  $sostituzione = str_replace($badword, '***', $testo);
+
+  // echo $sostituzione;
+
 ?>
